cfdd3d98ef1fe4b5720f290dc74f2c00364cf181
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-16 15:50:23 +0200

    adds styles and milliseconds

--- src/functions/create_watch_elements.ts
+++ src/functions/create_watch_elements.ts
@@ -1,9 +1,12 @@
 import append_to_app from "./append_to_app";
 import create_element from "./create_element";
 
-export default function create_watch_elements(): void {
-  const hours = create_element("div", "hours");
-  const minutes = create_element("div", "minutes");
-  const seconds = create_element("div", "seconds");
-  append_to_app(hours, minutes, seconds);
+export default function create_watch(): void {
+  const watch = create_element("section", "watch");
+  const hours = create_element("div", "hours bold");
+  const minutes = create_element("div", "minutes bold");
+  const seconds = create_element("div", "seconds bold");
+  const milliseconds = create_element("div", "milliseconds bold");
+  watch.append(hours, minutes, seconds, milliseconds);
+  append_to_app(watch);
 }
--- src/functions/watch_loop.ts
+++ src/functions/watch_loop.ts
@@ -2,12 +2,14 @@ export default function watch_loop() {
   const hoursElement = document.querySelector(".hours");
   const minutesElement = document.querySelector(".minutes");
   const secondsElement = document.querySelector(".seconds");
+  const millisecondsElement = document.querySelector(".milliseconds");
 
   setInterval(() => {
     const now = new Date();
-
     if (hoursElement) hoursElement.textContent = String(now.getHours());
     if (minutesElement) minutesElement.textContent = String(now.getMinutes());
     if (secondsElement) secondsElement.textContent = String(now.getSeconds());
+    if (millisecondsElement)
+      millisecondsElement.textContent = String(now.getMilliseconds());
   }, 1000);
 }
