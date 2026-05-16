4cb1e1b12b8f88af5f66aea0843a9533ed89ee1b
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-16 15:40:32 +0200

    changes to ts

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import "./style.css";
-import create_watch_elements from "./functions/create_watch_elements";
-import watch_loop from "./functions/watch_loop";
-
-create_watch_elements();
-watch_loop();
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import "./style.css";
+import create_watch_elements from "./functions/create_watch_elements.ts";
+import watch_loop from "./functions/watch_loop.ts";
+
+create_watch_elements();
+watch_loop();
