a8ad63290f0fbf237be9b4a7933456e8f63c5aa1
tahashieenavaz <tahashieenavaz@gmail.com>
2026-05-16 16:03:36 +0200

    pads all the watch elements

--- src/functions/watch_loop.ts
+++ src/functions/watch_loop.ts
@@ -6,11 +6,18 @@ export default function watch_loop() {
 
   requestAnimationFrame(function _watch_loop() {
     const now = new Date();
-    if (hoursElement) hoursElement.textContent = String(now.getHours());
-    if (minutesElement) minutesElement.textContent = String(now.getMinutes());
+    if (hoursElement) {
+      hoursElement.textContent = String(now.getHours()).padStart(2, "0");
+    }
+
+    if (minutesElement) {
+      minutesElement.textContent = String(now.getMinutes()).padStart(2, "0");
+    }
+
     if (secondsElement) {
       secondsElement.textContent = String(now.getSeconds()).padStart(2, "0");
     }
+
     if (millisecondsElement) {
       const ms = now.getMilliseconds();
       millisecondsElement.textContent = String(~~(ms / 10)).padStart(2, "0");
